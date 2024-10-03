722ebe45809da1722decf0ffa9e2b690a16287e0
Ayomide Bakare <ayomidebakare301@gmail.com>
2024-10-03 11:15:31 +0100

    Added fonts, side banner and smaller css to reymann

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "title": ["Noto Sans Variable", "sans-serif"],
+      "cabin": ["Cabin Variable", "ui-sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
